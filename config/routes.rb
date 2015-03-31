4230e8ce9f901c051db5bd353c54fba6860dca2b
AleksSenkou <aleksey.senkou@gmail.com>
2015-03-31 16:34:12 +0300

    update user model for adding identity options when signin with social network

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  match '/users/:id/finish_signup', to: 'users#finish_signup',
+                                    via: [:get, :patch],
+                                    as: :finish_signup
   root 'static_pages#home'
 end
