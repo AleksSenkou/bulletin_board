2f276d59515c069f806e2f97d22674f14e13d92c
AleksSenkou <aleksey.senkou@gmail.com>
2015-04-08 14:58:31 +0300

    add picture model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407092929) do
+ActiveRecord::Schema.define(version: 20150408115623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 20150407092929) do
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
+  create_table "pictures", force: :cascade do |t|
+    t.integer  "advert_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "pictures", ["advert_id"], name: "index_pictures_on_advert_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -60,4 +72,5 @@ ActiveRecord::Schema.define(version: 20150407092929) do
 
   add_foreign_key "adverts", "users"
   add_foreign_key "identities", "users"
+  add_foreign_key "pictures", "adverts"
 end
